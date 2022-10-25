e706eb554922a226ae7cc1dcaeb13aebb05f22f5
sl243 <slmiucse@gmail.com>
2022-10-26 01:41:23 +0600

    create categories data and send

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 
-// const categories = require('./data/categories.json')
+const categories = require('./data/categories.json')
 // const courses = require('./data/courses.json')
 
 app.get('/', (req, res) => {
@@ -16,9 +16,9 @@ app.get('/', (req, res) => {
 //     res.send(courses)
 // })
 
-// app.get('/course-categories', (req, res) => {
-//     res.send(categories)
-// })
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+})
 
 // app.get('/course/:id', (req, res) => {
 //     const id = req.params.id;
